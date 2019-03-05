a90487e0488291db55305a4192c4b583d3fb7c5d
yufrances90 <yufrances90@gmail.com>
2019-03-05 15:25:32 -0800

    feat: Recreated the server with Express

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,14 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express()
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const config = require('./config.json');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+const serverConfig = config["server"];
+const port = serverConfig["port"];
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
