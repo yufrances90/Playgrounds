c327c4e877a1c83eb251228ee49ce922d35f2429
yufrances90 <yufrances90@gmail.com>
2019-03-08 11:54:46 -0800

    feat: Implemented Haversine formula

---
+++ back-end/playgrounds_server/src/utils/helpers.js
@@ -0,0 +1,31 @@
+/*
+    Reference: 
+        https://www.movable-type.co.uk/scripts/latlong.html
+*/
+
+const toRadians = (deg) => {
+    return deg * (Math.PI / 180); 
+}
+
+const computeDistBtw2Coords = (location1, location2) => {
+
+    const { lng1, lat1 } = location1;
+    const { lng2, lat2 } = location2;
+
+    const R = 6371;
+
+    const dLat = toRadians(lat2 - lat1);
+    const dLng = toRadians(lng2 - lng1);
+
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
+                Math.sin(dLng / 2) * Math.sin(dLng / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c;
+}
+
+module.exports = {
+    computeDistBtw2Coords
+}
